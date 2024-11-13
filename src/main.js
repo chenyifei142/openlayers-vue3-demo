43782c83ab3251377ff1b046274a78a2bac557e0
chenyf <chenyifei142@gmail.com>
2024-11-13 11:11:12 +0800

    chore:add pinia-plugin-persistedstate

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import persistedState from 'pinia-plugin-persistedstate';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
 const pinia = createPinia()
+pinia.use(persistedState);
 const app = createApp(App)
 
 app.use(pinia)
