a4d5a33f7d8bb9d5f46d14c6bf3d7f03b80dd5f6
chenyf <chenyifei142@gmail.com>
2024-12-26 15:40:11 +0800

    fix:解决不显示当前鼠标位置问题

--- src/view/map/compoents/MapMousePosition.vue
+++ src/view/map/compoents/MapMousePosition.vue
@@ -11,37 +11,25 @@ const addMousePosition = () => {
     coordinateFormat: olCoordinate.createStringXY(4), // 坐标格式
     projection: "EPSG:4326", // 投影。默认值是视图投影
     className: "custom-mouse-position",
-    target: document.getElementById("custom-mouse-position"), // 如果您希望在地图的视区外呈现控件，请指定目标。
+    target: document.getElementById("map-mouse-position"), // 如果您希望在地图的视区外呈现控件，请指定目标。
     undefinedHTML: "&nbsp;",
   });
   map.value.addControl(mousePositionControl);
 }
 onMounted(async () => {
-  await nextTick(() => {
-    addMousePosition()
-  })
+  await nextTick()
+  console.log(map.value,"map")
+ await addMousePosition()
+  console.log(map.value,"map")
+
 });
 </script>
 
 <template>
-  <div id="map-mouse-position" class="mouse-position-container">
-    <div id="custom-mouse-position" class="custom-mouse-position">&nbsp;</div>
+  <div id="map-mouse-position">
+    <div class="custom-mouse-position">&nbsp;</div>
   </div>
 </template>
 
 <style scoped>
-.mouse-position-container {
-  position: absolute;
-  bottom: 10px; /* 设置鼠标位置控件容器的位置，例如在地图容器的底部 */
-  left: 10px;
-  z-index: 9999;
-}
-
-.custom-mouse-position {
-  color: #333;
-  font-size: 12px;
-  background-color: rgba(255, 255, 255, 0.7); /* 背景色，方便阅读 */
-  border-radius: 4px; /* 圆角 */
-  text-align: center;
-}
 </style>
