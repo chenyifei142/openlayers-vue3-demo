7aea352ec051ed4d17528afb69304398e4ed69ab
chenyf <chenyifei142@gmail.com>
2024-12-27 17:14:18 +0800

    fix:去除无意义节流

--- src/view/map/compoents/layer/MapLayerYangtzeRiverDelta.vue
+++ src/view/map/compoents/layer/MapLayerYangtzeRiverDelta.vue
@@ -12,6 +12,7 @@ import img from '@/assets/img/3ccb370241b24348a44c2f44bd480837.png'
 // import img from '@/assets/img/b3040999a20d4684be34b95c6c87df2c.png'
 // import img from '@/assets/img/c983cbb8620640308c094fbaad2d6d59.png'
 import {transformExtent} from "ol/proj.js";
+
 const map = inject('map')
 const imgLayer = ref(null)
 const {createLayer, setImgLayer, removeImgLayer, setImgLayerOpacity} = useImgLayer(imgLayer)
@@ -19,27 +20,18 @@ const {createLayer, setImgLayer, removeImgLayer, setImgLayerOpacity} = useImgLay
 const csj_layerVector = useCreateVectorLayer(ChangJiangDeltaGeoJson, stylesMap.csjStyle, 716)
 olUtils.addFeaturesByUrl(csj_layerVector.value, ShangHaiGeoJson)
 
-// 更新图层的可见性
-const updateLayerVisibility = olUtils.throttle(() => {
-  const zoomLevel = map.value.getView().getZoom(); // 获取当前缩放等级
-  if (zoomLevel >= 9) csj_layerVector.value.setVisible(false)
-  else csj_layerVector.value.setVisible(true);
-}, 200); // 200 毫秒的节流时间
-
 onMounted(() => {
   nextTick(() => {
     map.value.addLayer(csj_layerVector.value)
     createLayer()
     //  extent 数组中的四个坐标（左、下、右、上）表示的是一个矩形区域的边界
-    setImgLayer(img,  [72.502355, 6.516187, 135.50567, 56.80269]);
+    setImgLayer(img, [72.502355, 6.516187, 135.50567, 56.80269]);
     // setImgLayer(img,  [73.291857, 13.2689, 135.22275, 53.756187]);
-    console.log(csj_layerVector.value.getSource().getExtent(),"csj_layerVector.value.getSource().getExtent()")
     const extent3857 = [8392583.131199298, 2057327.4948680997, 14774628.946682949, 6846580.563414273];
 
-// 将范围从 EPSG:3857 转换为 EPSG:4326
+    // 将范围从 EPSG:3857 转换为 EPSG:4326
     const extent4326 = transformExtent(extent3857, 'EPSG:3857', 'EPSG:4326');
-    console.log(extent4326,"extent4326")
-    map.value.on('moveend', updateLayerVisibility);
+    console.log(extent4326, "extent4326")
   })
 })
 </script>
